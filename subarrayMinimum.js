f732004819c02b09b74ba6eaa928c4b74b52206d
Alex Tsiras <tsirasalexis@yahoo.gr>
2015-08-17 17:35:21 +0300

    Fixed minValue error.

--- subarrayMinimum.js
+++ subarrayMinimum.js
@@ -6,10 +6,11 @@ var indexOfMinimum = function(array, startIndex) {
 
     // Loop over items starting with startIndex, 
     // updating minValue and minIndex as needed:
-    for (var i = minIndex +1 ; i < array.length - minIndex; i++) {
-        if (array[i] > array[i+1]){
-            minIndex = i+1;
-            minValue = array[i+1];
+    for (var i = minIndex +1 ; i < array.length; i++) {
+        if (array[i] < array[minIndex]){
+            minIndex = i;
+            minValue = array[i];
+            //println(minValue + " " + minIndex);
         }   
     }
     return minIndex;
@@ -23,4 +24,8 @@ var index = indexOfMinimum(array, 2);
 //  Since 9 is at index 4 in the original array, 
 //  "index" has value 4
 println("The index of the minimum value of the subarray starting at index 2 is " + index + "."  );
+Program.assertEqual(index, 4);
+var index = indexOfMinimum(array, 0);
+Program.assertEqual(index, 1);
+var index = indexOfMinimum(array, 3);
 Program.assertEqual(index, 4);
